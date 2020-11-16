b6f4bd624922ef14a012a8285b23bf73d2b5e897
pirosveta <svetiklanka.2000@yandex.ru>
2020-11-16 14:10:53 +0300

    Scripted auto-commit on change (2020-11-16 14:10:53) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/StoreActor.java
+++ src/main/java/ru/bmstu/akka/StoreActor.java
@@ -1,4 +1,10 @@
 package ru.bmstu.akka;
 
-public class StoreActor {
+import akka.actor.AbstractActor;
+
+public class StoreActor extends AbstractActor {
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
