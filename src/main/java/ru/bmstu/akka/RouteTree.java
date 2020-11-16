128acc34b1b66e2afa734c498491bd19704849e2
pirosveta <svetiklanka.2000@yandex.ru>
2020-11-16 14:14:22 +0300

    Scripted auto-commit on change (2020-11-16 14:14:22) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/RouteTree.java
+++ src/main/java/ru/bmstu/akka/RouteTree.java
@@ -1,5 +1,24 @@
 package ru.bmstu.akka;
 
+import akka.actor.ActorSystem;
+import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
+
 public class RouteTree {
-    
+    ActorSystem system = ActorSystem.create("routes");
+    final Http http = Http.get(system);
+    final ActorMaterializer materializer = ActorMaterializer.create(system);
+    MainHttp instance = new MainHttp(system);
+    final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+            instance.createRoute(system).flow(system, materializer);
+    final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+            routeFlow,
+            ConnectHttp.toHost("localhost", 8080),
+            materializer
+    );
+System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+System.in.read();
+binding
+        .thenCompose(ServerBinding::unbind)
+            .thenAccept(unbound -> system.terminate());
 }
