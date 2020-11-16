f4fee386b54cf56cc24ef1ec850d3bfaa199fa8a
pirosveta <svetiklanka.2000@yandex.ru>
2020-11-16 17:35:39 +0300

    Scripted auto-commit on change (2020-11-16 17:35:39) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/RouteTree.java
+++ src/main/java/ru/bmstu/akka/RouteTree.java
@@ -10,6 +10,27 @@ public class RouteTree {
         ActorSystem system = ActorSystem.create("routes");
         final ActorMaterializer materializer = ActorMaterializer.create(system);
 
-        Route route = 
+        Route route = path("semaphore", () ->
+                route(
+                        get( () -> {
+                            Future<Object> result = Patterns.ask(testPackageActor,
+                                    SemaphoreActor.makeRequest(), 5000);
+                            return completeOKWithFuture(result, Jackson.marshaller());
+                        }))),
+        path("test", () ->
+                route(
+                        post(() ->
+                                entity(Jackson.unmarshaller(TestPackageMsg.class), msg -> {
+                                    testPackageActor.tell(msg, ActorRef.noSender());
+                                    return complete("Test started!");
+                                })))),
+                path("put", () ->
+                        get(() ->
+                                parameter("key", (key) ->
+                                        parameter("value", (value) ->
+                                        {
+                                            storeActor.tell(new StoreActor.StoreMessage(key, value), ActorRef.noSender());
+                                            return complete("value saved to store ! key=" + key + " value=" + value);
+                                        })))),
     }
 }
