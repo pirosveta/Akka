e0ba033fcdc58ecab597307175dd4fc0d3c6a284
pirosveta <svetiklanka.2000@yandex.ru>
2020-11-16 14:11:12 +0300

    Scripted auto-commit on change (2020-11-16 14:11:12) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/ExecuteActor.java
+++ src/main/java/ru/bmstu/akka/ExecuteActor.java
@@ -1,4 +1,10 @@
 package ru.bmstu.akka;
 
-public class ExecuteActor {
+import akka.actor.AbstractActor;
+
+public class ExecuteActor extends AbstractActor {
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
