07191e94d1065a6a6c13031ae4ba35cc5ab0b415
pirosveta <svetiklanka.2000@yandex.ru>
2020-11-16 18:03:43 +0300

    Scripted auto-commit on change (2020-11-16 18:03:43) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/MainHttp.java
+++ src/main/java/ru/bmstu/akka/MainHttp.java
@@ -1,11 +1,16 @@
 package ru.bmstu.akka;
 
+import akka.NotUsed;
 import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 
 import java.util.concurrent.CompletionStage;
 
@@ -30,5 +35,9 @@ public class MainHttp extends AllDirectives {
         binding
                 .thenCompose(ServerBinding::unbind)
                 .thenAccept(unbound -> system.terminate());
+    }
+
+    private Route createRoute(ActorSystem system) {
+        return null;
     }
 }
