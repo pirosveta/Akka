8ee2cdc1bd4992a985870719d718d23dc15e7386
pirosveta <svetiklanka.2000@yandex.ru>
2020-11-16 14:18:49 +0300

    Scripted auto-commit on change (2020-11-16 14:18:49) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/RouteTree.java
+++ src/main/java/ru/bmstu/akka/RouteTree.java
@@ -13,20 +13,24 @@ import akka.stream.javadsl.Flow;
 import java.util.concurrent.CompletionStage;
 
 public class RouteTree {
-    ActorSystem system = ActorSystem.create("routes");
-    final Http http = Http.get(system);
-    final ActorMaterializer materializer = ActorMaterializer.create(system);
-    MainHttp instance = new MainHttp(system);
-    final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
-            instance.createRoute(system).flow(system, materializer);
-    final CompletionStage<ServerBinding> binding = http.bindAndHandle(
-            routeFlow,
-            ConnectHttp.toHost("localhost", 8080),
-            materializer
-    );
-    System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
-    System.in.read();
-    binding
-            .thenCompose(ServerBinding::unbind)
-                .thenAccept(unbound -> system.terminate());
+    public static void main(String[] args) {
+        ActorSystem system = ActorSystem.create("routes");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        MainHttp instance = new MainHttp(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+                instance.createRoute(system).flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .
+
+                        thenAccept(unbound -> system.terminate());
+    }
 }
