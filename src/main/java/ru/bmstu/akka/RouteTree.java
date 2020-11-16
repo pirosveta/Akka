b84c0ce212dcff9374a99cc18cecf86a3890ec44
pirosveta <svetiklanka.2000@yandex.ru>
2020-11-16 16:22:33 +0300

    Scripted auto-commit on change (2020-11-16 16:22:33) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/RouteTree.java
+++ src/main/java/ru/bmstu/akka/RouteTree.java
@@ -4,12 +4,10 @@ import akka.actor.ActorSystem;
 import akka.http.javadsl.Http;
 import akka.stream.ActorMaterializer;
 
-import java.io.IOException;
-
 public class RouteTree {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("routes");
-        final Http http = Http.get(system);
+        
         final ActorMaterializer materializer = ActorMaterializer.create(system);
     }
 }
