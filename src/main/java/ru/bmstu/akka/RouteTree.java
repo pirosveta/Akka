6f35f318bc316b3a49ee23ce02353330c3292399
pirosveta <svetiklanka.2000@yandex.ru>
2020-11-16 17:59:42 +0300

    Scripted auto-commit on change (2020-11-16 17:59:42) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/RouteTree.java
+++ src/main/java/ru/bmstu/akka/RouteTree.java
@@ -5,26 +5,30 @@ import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
+import akka.stream.ActorMaterializer;
 
 import java.util.concurrent.CompletionStage;
 
 public class MainHttp extends AllDirectives {
+    public MainHttp(ActorSystem system) {
+    }
+
     public static void main(String[] args) throws Exception {
         ActorSystem system = ActorSystem.create("routes");
-
         final Http http = Http.get(system);
-
-        HttpServerMinimalExampleTest app = new HttpServerMinimalExampleTest();
-
-        final CompletionStage binding =
-                http.newServerAt("localhost", 8080)
-                        .bind(app.createRoute());
-
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        MainHttp instance = new MainHttp(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+                instance.createRoute(system).flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
         System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
-        System.in.read(); // let it run until user presses return
-
+        System.in.read();
         binding
-                .thenCompose(ServerBinding::unbind) // trigger unbinding from the port
-                .thenAccept(unbound -> system.terminate()); // and shutdown when done
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
     }
 }
