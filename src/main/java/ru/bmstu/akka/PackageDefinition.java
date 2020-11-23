31cf918b9faee4f35e5c20178c9a8f81b0104816
pirosveta <svetiklanka.2000@yandex.ru>
2020-11-23 17:42:03 +0300

    Scripted auto-commit on change (2020-11-23 17:42:03) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/PackageDefinition.java
+++ src/main/java/ru/bmstu/akka/PackageDefinition.java
@@ -6,15 +6,19 @@ import com.fasterxml.jackson.databind.JsonNode;
 import java.util.List;
 
 public class PackageDefinition {
-    private final String packageID, jsScript, functionName;
+    private final String packageID;
+    private final String jsScript;
+    private final String functionName;
     private final List<JsonNode> tests;
 
-    public PackageDefinition(@JsonProperty("path") String path,
-                          @JsonProperty("requests") List<JsonNode> requests,
-                          @JsonProperty("responses") List<JsonNode> responses) {
-        this.path = path;
-        this.requests = requests;
-        this.responses = responses;
+    public PackageDefinition(@JsonProperty("packageID") String packageID,
+                          @JsonProperty("jsScript") String jsScript,
+                          @JsonProperty("functionName") String functionName,
+                          @JsonProperty("tests") List<JsonNode> tests) {
+        this.packageID = packageID;
+        this.jsScript = jsScript;
+        this.functionName = functionName;
+        this.tests = tests;
     }
 
     public String getPath() {
