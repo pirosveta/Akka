d147adb7dda3a5359610df80b88edb77eedb1f41
pirosveta <svetiklanka.2000@yandex.ru>
2020-11-23 23:34:54 +0300

    Scripted auto-commit on change (2020-11-23 23:34:54) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/Kernel.java
+++ src/main/java/ru/bmstu/akka/Kernel.java
@@ -9,7 +9,8 @@ import akka.routing.SmallestMailboxPool;
 public class Kernel extends AbstractActor {
     @Override
     public Receive createReceive() {
-        ActorRef storeRouter = getContext().actorOf(new SmallestMailboxPool(5).props(Props.create(StoreActor.class, "storeActor")));
+        ActorRef storeRouter = getContext().actorOf(new SmallestMailboxPool(5).props(Props.create(StoreActor.class)), "store");
+        ActorRef executeRouter = getContext().actorOf(new SmallestMailboxPool(5).props(Props.create(ExecuteActor.class)), "execute");
         return ReceiveBuilder.create()
                 .match(PackageDefinition.class, pack -> {
 
