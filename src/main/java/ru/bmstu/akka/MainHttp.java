a1a81aa0b656c3c4220762c4a68fa6660516df2b
pirosveta <svetiklanka.2000@yandex.ru>
2020-11-23 22:31:42 +0300

    Scripted auto-commit on change (2020-11-23 22:31:42) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/MainHttp.java
+++ src/main/java/ru/bmstu/akka/MainHttp.java
@@ -39,7 +39,9 @@ public class MainHttp extends AllDirectives {
         );
         System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
         System.in.read();
-        binding.thenCompose(ServerBinding::unbind).thenAccept(unbound -> system.terminate());
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
     }
 
     private Route createRoute(ActorSystem system) {
