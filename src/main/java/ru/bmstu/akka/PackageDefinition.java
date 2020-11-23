f4e370afa590c99147eb1bc0e4a9974ae8252360
pirosveta <svetiklanka.2000@yandex.ru>
2020-11-23 17:37:19 +0300

    Scripted auto-commit on change (2020-11-23 17:37:19) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/PackageDefinition.java
+++ src/main/java/ru/bmstu/akka/PackageDefinition.java
@@ -1,4 +1,27 @@
 package ru.bmstu.akka;
 
 public class PackageDefinition {
+    private final String path;
+    private final List<JsonNode> requests;
+    private final List<JsonNode> responses;
+
+    public MockDefinition(@JsonProperty("path") String path,
+                          @JsonProperty("requests") List<JsonNode> requests,
+                          @JsonProperty("responses") List<JsonNode> responses) {
+        this.path = path;
+        this.requests = requests;
+        this.responses = responses;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public List<JsonNode> getRequests() {
+        return requests;
+    }
+
+    public List<JsonNode> getResponses() {
+        return responses;
+    }
 }
