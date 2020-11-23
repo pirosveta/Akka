8d46e8cf81bbec8e6f44948a1d3d882225c308b8
pirosveta <svetiklanka.2000@yandex.ru>
2020-11-23 17:38:08 +0300

    Scripted auto-commit on change (2020-11-23 17:38:08) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/PackageDefinition.java
+++ src/main/java/ru/bmstu/akka/PackageDefinition.java
@@ -1,9 +1,7 @@
 package ru.bmstu.akka;
 
 public class PackageDefinition {
-    private final String packageID;
-    private final List<JsonNode> jsScript;
-    private final List<JsonNode> functionName;
+    private final String packageID, jsScript, functionName;
 
     public MockDefinition(@JsonProperty("path") String path,
                           @JsonProperty("requests") List<JsonNode> requests,
