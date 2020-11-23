4aa5fe6adac21b48147609812d9bcc0e68bfedd3
pirosveta <svetiklanka.2000@yandex.ru>
2020-11-23 17:46:23 +0300

    Scripted auto-commit on change (2020-11-23 17:46:23) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/MainHttp.java
+++ src/main/java/ru/bmstu/akka/MainHttp.java
@@ -40,7 +40,7 @@ public class MainHttp extends AllDirectives {
 
     private Route createRoute(ActorSystem system) {
         return post(() ->
-            entity(Jackson.unmarshaller(PackageDefinition.class), )
+            entity(Jackson.unmarshaller(PackageDefinition.class), pack ->)
         );
     }
 }
