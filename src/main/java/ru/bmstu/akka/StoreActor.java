48075489233ff348593930ee657731b95e864115
pirosveta <svetiklanka.2000@yandex.ru>
2020-11-24 15:11:34 +0300

    Scripted auto-commit on change (2020-11-24 15:11:34) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/StoreActor.java
+++ src/main/java/ru/bmstu/akka/StoreActor.java
@@ -1,9 +1,14 @@
 package ru.bmstu.akka;
 
 import akka.actor.AbstractActor;
+import akka.japi.Pair;
 import akka.japi.pf.ReceiveBuilder;
 
+import java.util.Map;
+
 public class StoreActor extends AbstractActor {
+    Map<String, Map<String, List<>>>
+
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create().matchAny(input -> {
