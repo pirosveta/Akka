d7dd10f4c6d27e7a2c66fcef5c462ab8507ece67
pirosveta <svetiklanka.2000@yandex.ru>
2020-11-24 16:14:05 +0300

    Scripted auto-commit on change (2020-11-24 16:14:05) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/ExecuteActor.java
+++ src/main/java/ru/bmstu/akka/ExecuteActor.java
@@ -1,10 +1,20 @@
 package ru.bmstu.akka;
 
 import akka.actor.AbstractActor;
+import akka.japi.Pair;
+import akka.japi.pf.ReceiveBuilder;
+
+import java.util.List;
+import java.util.Map;
 
 public class ExecuteActor extends AbstractActor {
     @Override
     public Receive createReceive() {
-        return null;
+        return ReceiveBuilder.create().matchAny(input -> {
+            Pair<PackageDefinition, TestsDefinition> pair = (Pair<PackageDefinition, TestsDefinition>) input;
+            PackageDefinition pack = pair.first();
+            TestsDefinition test = pair.second();
+        })
+        .build();
     }
 }
