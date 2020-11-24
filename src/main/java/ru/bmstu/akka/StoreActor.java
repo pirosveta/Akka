7c3e8045daf1aceae4f30d951bcb13f90d2f7c49
pirosveta <svetiklanka.2000@yandex.ru>
2020-11-24 15:29:24 +0300

    Scripted auto-commit on change (2020-11-24 15:29:24) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/StoreActor.java
+++ src/main/java/ru/bmstu/akka/StoreActor.java
@@ -3,6 +3,7 @@ package ru.bmstu.akka;
 import akka.actor.AbstractActor;
 import akka.japi.Pair;
 import akka.japi.pf.ReceiveBuilder;
+import com.fasterxml.jackson.databind.JsonNode;
 
 import java.util.List;
 import java.util.Map;
@@ -15,7 +16,14 @@ public class StoreActor extends AbstractActor {
     public Receive createReceive() {
         return ReceiveBuilder.create().matchAny(input -> {
             PackageDefinition pack = (PackageDefinition) input;
-            
+            Map<String, List<String>> tests= results.get(pack.getPackageID());
+            List<JsonNode> testsName = pack.getTests();
+            List<JsonNode> testsValue = pack.get
+            for (int i = 0; i < testsName.size(); i++) {
+                String name = testsName.get(i).toString();
+                String value =
+                results.put(pack.getPackageID(), tests.put(j.toString(), ))
+            }
         })
         .build();
     }
