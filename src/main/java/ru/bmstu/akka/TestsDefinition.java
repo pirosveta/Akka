a8af671e96e8744b31e412f298ed57931fe0d191
pirosveta <svetiklanka.2000@yandex.ru>
2020-11-24 15:31:02 +0300

    Scripted auto-commit on change (2020-11-24 15:31:02) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/TestsDefinition.java
+++ src/main/java/ru/bmstu/akka/TestsDefinition.java
@@ -1,2 +1,23 @@
+package ru.bmstu.akka;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.util.List;
+
 public class TestsDefinition {
+    private final String packageID;
+    private final String jsScript;
+    private final String functionName;
+    private final List<JsonNode> tests;
+
+    public PackageDefinition(@JsonProperty("packageID") String packageID,
+                             @JsonProperty("jsScript") String jsScript,
+                             @JsonProperty("functionName") String functionName,
+                             @JsonProperty("tests") List<JsonNode> tests) {
+        this.packageID = packageID;
+        this.jsScript = jsScript;
+        this.functionName = functionName;
+        this.tests = tests;
+    }
 }
