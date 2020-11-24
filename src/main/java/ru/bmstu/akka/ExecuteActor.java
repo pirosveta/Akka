8ab22314189cc1880c9c7542b616bb2796b7eeaf
pirosveta <svetiklanka.2000@yandex.ru>
2020-11-24 16:20:15 +0300

    Scripted auto-commit on change (2020-11-24 16:20:15) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/ExecuteActor.java
+++ src/main/java/ru/bmstu/akka/ExecuteActor.java
@@ -4,6 +4,9 @@ import akka.actor.AbstractActor;
 import akka.japi.Pair;
 import akka.japi.pf.ReceiveBuilder;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
 import java.util.List;
 import java.util.Map;
 
@@ -14,7 +17,10 @@ public class ExecuteActor extends AbstractActor {
             Pair<PackageDefinition, TestsDefinition> pair = (Pair<PackageDefinition, TestsDefinition>) input;
             PackageDefinition pack = pair.first();
             TestsDefinition test = pair.second();
-            
+            ScriptEngine engine = new ScriptEngineManager().getEngineByName("executeTests");
+            engine.eval(pack.getJsScript());
+            Invocable invocable = (Invocable) engine;
+            return invocable.invokeFunction(pack.getFunctionName(), ).toString();
         })
         .build();
     }
