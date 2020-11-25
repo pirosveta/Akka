d4a47c0de603fb6a8fb121ef2c57daab054b58e3
pirosveta <svetiklanka.2000@yandex.ru>
2020-11-25 12:53:57 +0300

    Scripted auto-commit on change (2020-11-25 12:53:57) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/MainHttp.java
+++ src/main/java/ru/bmstu/akka/MainHttp.java
@@ -22,11 +22,17 @@ import java.util.concurrent.CompletionStage;
 
 public class MainHttp extends AllDirectives {
 
+    private ActorRef kernel;
+
+    public MainHttp(ActorSystem system) {
+        kernel = system.actorOf(Props.create(Kernel.class));
+    }
+
     public static void main(String[] args) throws Exception {
         ActorSystem system = ActorSystem.create("routes");
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-        MainHttp instance = new MainHttp();
+        MainHttp instance = new MainHttp(system);
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
                 instance.createRoute(system).flow(system, materializer);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
@@ -42,7 +48,6 @@ public class MainHttp extends AllDirectives {
     }
 
     private Route createRoute(ActorSystem system) {
-        ActorRef kernel = system.actorOf(Props.create(Kernel.class));
         return route(
                 post(() ->
                     entity(Jackson.unmarshaller(PackageDefinition.class), pack ->
