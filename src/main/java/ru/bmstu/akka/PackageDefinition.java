c271dc40d3f2f5f701679ba98403b7a036745b55
pirosveta <svetiklanka.2000@yandex.ru>
2020-12-24 15:29:46 +0300

    Scripted auto-commit on change (2020-12-24 15:29:46) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/PackageDefinition.java
+++ src/main/java/ru/bmstu/akka/PackageDefinition.java
@@ -5,15 +5,18 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 public class PackageDefinition {
+    private final String PACKAGE_ID_PATH = "packageId", JS_SCRIPT_PATH = "jsScript",
+            FUNCTION_NAME_PATH = "functionName", TESTS_PATH = "tests";
+
     private final String packageId;
     private final String jsScript;
     private final String functionName;
     private final List<TestsDefinition> tests;
 
-    public PackageDefinition(@JsonProperty("packageId") String packageId,
-                             @JsonProperty("jsScript") String jsScript,
-                             @JsonProperty("functionName") String functionName,
-                             @JsonProperty("tests") List<TestsDefinition> tests) {
+    public PackageDefinition(@JsonProperty(PACKAGE_ID_PATH) String packageId,
+                             @JsonProperty(JS_SCRIPT_PATH) String jsScript,
+                             @JsonProperty(FUNCTION_NAME_PATH) String functionName,
+                             @JsonProperty(TESTS_PATH) List<TestsDefinition> tests) {
         this.packageId = packageId;
         this.jsScript = jsScript;
         this.functionName = functionName;
